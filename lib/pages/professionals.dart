cb9aa8475585945634d3cc1b767fd4752f840020
Ayush <ayku21cs@cmrit.ac.in>
2023-04-09 23:36:16 +0530

    Two ss of app added for the readme file

--- lib/pages/professionals.dart
+++ lib/pages/professionals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class professionals extends StatefulWidget {
   const professionals({Key? key}) : super(key: key);
@@ -13,56 +14,63 @@ class _professionalsState extends State<professionals> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-        body: Padding(
-          padding: const EdgeInsets.only(left: 14,right: 14),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children:[
-                SizedBox(
-                  height: 50,
-                ),
-              Text("Mentors",style: TextStyle(
-                fontWeight:FontWeight.bold,
-                fontSize: 34,color: Colors.white
-              ),),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 14, right: 14),
+        child: SingleChildScrollView(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Mentors",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 34,
+                  color: Colors.white),
+            ),
+            Stack(children: [
               Padding(
-                padding: const EdgeInsets.symmetric(vertical: 40.0,horizontal: 40),
+                padding: const EdgeInsets.symmetric(vertical: 28.0),
                 child: Container(
-                  height:50,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("allimagess/img9.png"),
+                        fit: BoxFit.cover
+                    ),
+                    color: Colors.grey,
+                  ),
+                  height: 250,
+
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 40.0, horizontal: 40),
+                child: Container(
+                  height: 50,
                   decoration: BoxDecoration(
                       color: Colors.black,
                       borderRadius: BorderRadius.circular(10),
                       border: Border.all(
-                          color: Colors.indigoAccent.shade100,
-                          width: 2
-                      )
-                  ),
+                          color: Colors.indigoAccent.shade100, width: 2)),
                   child: Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 18.0),
                     child: TextField(
                       maxLines: 1,
-                      style: TextStyle(
-                          color: Colors.white
-                      ),
+                      style: TextStyle(color: Colors.white),
                       decoration: InputDecoration(
                           border: InputBorder.none,
                           hintText: "Search for you mentor by name or category",
-                          hintStyle: TextStyle(
-                              color: Colors.white54
-                          )
-                      ),
+                          hintStyle: TextStyle(color: Colors.white54)),
                     ),
                   ),
                 ),
               ),
-                Container(
-                  
-                )
-    ]
-            ),
-          ),
+            ]),
+          ]),
         ),
+      ),
     );
   }
 }
